c0a84c64c6e56d11665139f9176db475c1a9bb9e
virgil elliot <115853295+vejtheguy@users.noreply.github.com>
2025-04-13 16:06:45 -0700

    Move images into static folder to update background.

--- src/lib/components/Background.svelte
+++ src/lib/components/Background.svelte
@@ -13,7 +13,7 @@
 
 {#key num}
 	<div
-		style="background-image: url('src/lib/imgs/img{num}.jpg')"
+		style="background-image: url('/imgs/img{num}.jpg')"
 		transition:fade={{ duration: 1500 }}
 		class="absolute z-[-1] min-h-screen w-full bg-cover bg-center bg-no-repeat"
 	></div>
