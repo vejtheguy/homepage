899962bb5e8d105f7c3b47e7bb5e265936b9a446
virgil elliot <115853295+vejtheguy@users.noreply.github.com>
2025-04-13 19:41:11 -0700

    Update: random numbers always different & one image load at start

--- src/lib/components/Background.svelte
+++ src/lib/components/Background.svelte
@@ -1,20 +1,39 @@
-<script>
+<script lang="ts">
 	import { fade } from 'svelte/transition';
 	import HourTimer from './HourTimer.svelte';
+	import { onMount } from 'svelte';
+
+	let num: number;
+	let hasMounted = false;
+
+	const getNum = () => {
+		let newNum = randomNum();
+
+		while (newNum === num) {
+			newNum = randomNum();
+		}
+
+		return newNum;
+	};
 
 	const randomNum = () => {
 		return Math.floor(Math.random() * 6) + 1;
 	};
 
-	let num = randomNum();
+	onMount(() => {
+		num = getNum();
+		hasMounted = true;
+	});
 </script>
 
-<HourTimer on:tick={() => (num = randomNum())} />
+<HourTimer on:tick={() => (num = getNum())} />
 
-{#key num}
-	<div
-		style="background-image: url('/imgs/img{num}.jpg')"
-		transition:fade={{ duration: 1500 }}
-		class="absolute z-[-1] min-h-screen w-full bg-cover bg-center bg-no-repeat"
-	></div>
-{/key}
+{#if hasMounted}
+	{#key num}
+		<div
+			style="background-image: url('/imgs/img{num}.jpg')"
+			transition:fade={{ duration: 1500 }}
+			class="absolute z-[-1] min-h-screen w-full bg-cover bg-center bg-no-repeat"
+		></div>
+	{/key}
+{/if}
